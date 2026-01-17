d2a9ca77acabdb7a04b93121b967763737bde349
rahatul-51007 <rahatul51007@gmail.com>
2026-01-17 10:45:00 +0600

    adnin dashboard complete

--- model/room_price_query.php
+++ model/room_price_query.php
@@ -7,4 +7,9 @@ $result = mysqli_query($conn, $query);
 
 $query2= "SELECT COUNT(*) AS total_staff FROM user WHERE role='employee'";
 $result2 =mysqli_query($conn, $query2);
+
+$query3= "SELECT COUNT(*) AS total_booked FROM booking WHERE status='approved'";
+$result3 =mysqli_query($conn, $query3);
+
+
 ?>
--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -24,12 +24,13 @@ include_once 'nav.php';
             </ul>
          </div>
         <!-- div dashboard -->
-         <div class="dashboard">
-            
+         <div class="dashboard">           
             <div id="home" >
                 <div class="count">
                    <div class="count-card">
+                        <?php $row = mysqli_fetch_assoc($result3); ?>
                         <p class="count-title">Total Booked Room</p>
+                        <h1><?php echo $row['total_booked']; ?></h1>
                     </div>
                     <div class="count-card"> 
                         <?php $row = mysqli_fetch_assoc($result2); ?>
@@ -47,85 +48,6 @@ include_once 'nav.php';
                     <?php } ?>
                 </div>
             </div>
-            <!-- <div id="update" >
-                <div class="form-room" >
-                    <form action="" id="room-option" >
-                        <table>
-                            <tr>
-                                <td>Room Type</td>
-                                <td>
-                                    <select id="">
-                                        <option value="">--Select--</option>
-                                        <option value="Delux">Delux</option>
-                                        <option value="Super Delux King">Super Delux King</option>
-                                        <option value="Super Delux Twin">Super Delux Twin</option>
-                                        <option value="Junior Suit">Junior Suit</option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Quantity</td>
-                                <td><input type="text"></td>
-                            </tr>
-                            <tr>
-                                <td>Price</td>
-                                <td><input type="text"></td>
-                            </tr>
-                        </table>
-                        <input type="submit" value="Save" class="save">
-                    </form>
-                </div>
-            </div> -->
-            <!-- <div id="add-remove">
-                <div class="form-add">
-                    <form action="../controllers/addEmployeeController.php" id="add-option" method="post" >
-                        <table>
-                            <tr>
-                                <td>Full name</td>
-                                <td><input type="text"></td>                                </td>
-                            </tr>
-                            <tr>
-                                <td>Username</td>
-                                <td><input type="text"></td>                                </td>
-                            </tr>
-                            <tr>
-                                <td>Mobile</td>
-                                <td><input type="tel"></td>                                </td>
-                            </tr>
-                            <tr>
-                                <td>Password</td>
-                                <td><input type="text"></td>
-                            </tr>
-                        </table>
-                        <input type="submit" value="Add" class="add">
-                    </form>
-                </div>     
-            </div> -->
-            <!-- <div id="staff">
-            </div> -->
-            <!-- <div id="summary">
-                <div class="search-container">
-                    <form action="" id="search-by-room-name">
-                        <input type="text">
-                        <input type="submit" class="search-rname" value="Search" >
-                    </form>
-                    <form action="" id="search-by-date">
-                        <table>
-                            <tr>
-                                <td>Start Date</td>
-                                <td><input type="date" name="" id=""></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>End Date</td>
-                                <td><input type="Date"></td>
-                                <td><input type="submit" class="search-date" value="Search"></td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-
-            </div> -->
          </div>
     </div>
     <script src="./js/script_admin.js"></script>
