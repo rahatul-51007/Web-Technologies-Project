adddeb57320d55adc0020ca6a50e6b16db25ec3c
rahatul-51007 <rahatul51007@gmail.com>
2026-01-17 10:45:56 +0600

    title added

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -8,7 +8,7 @@ include_once 'nav.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin Dashboard</title>
     <link rel="stylesheet" href="./css/admin_style.css">
 </head>
 <body>  
