a323625ea42f7f4e6372e7568636045b83f98d80
hedayet-ullah-patwary <hedayetullahpatwary@gmail.com>
2026-01-18 21:29:59 +0600

    Add registration controller

---
+++ controllers/reg_controller.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require_once '../model/reg_query.php';
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === "POST") {
+    $name = $_POST['name'];
+    $username = $_POST['uname'];
+    $email = $_POST['email'];
+    $password = $_POST['pwd'];
+    $_SESSION['nameErrMsg'] = "";
+    $_SESSION['usernameErrMsg'] = "";
+    $_SESSION['emailErrMsg'] = "";
+    $_SESSION['passwordErrMsg'] = "";
+    $_SESSION['name'] = "";
+    $_SESSION['username'] = "";
+    $_SESSION['email'] = "";
+    $_SESSION['pwd'] = "";
+    $isValid = true;
+
+    if (empty($name)) {
+        $_SESSION['nameErrMsg'] = "Name is empty";
+        $_SESSION['success'] = "";
+        $isValid = false;
+    } else {
+        $_SESSION['name'] = $name;
+    }
+
+    if (empty($username)) {
+        $_SESSION['usernameErrMsg'] = "Username is empty";
+        $_SESSION['success'] = "";
+        $isValid = false;
+    } else {
+        $_SESSION['username'] = $username;
+    }
+    if (empty($email)) {
+        $_SESSION['emailErrMsg'] = "Email is empty";
+        $_SESSION['success'] = "";
+        $isValid = false;
+    } else {
+        $_SESSION['email'] = $email;
+    }
+    if (empty($password)) {
+        $_SESSION['passwordErrMsg'] = "Password is empty";
+        $_SESSION['success'] = "";
+        $isValid = false;
+    } 
+    else {
+        $_SESSION['password'] = $password;
+    }
+    if ($isValid) {
+        $user = [
+            "name" => $name,
+            "uname" => $username,
+            "email" => $email,
+            "pwd" => $password
+        ];
+        regController($user);
+    } else {
+        header("Location: ../views/reg.php");
+        exit();
+    }
+} else {
+    echo "Something went wrong. Please contact the administrator";
+}
+
+function regController($user)
+{
+    $status = regUser($user);
+    $_SESSION['invalidUnameMsg'] = "";
+    $_SESSION['invalidEmailMsg'] = "";
+    $_SESSION['invalidMsg'] = "";
+    $_SESSION['success'] = "";
+    if ($status === true) {
+        $_SESSION['success'] = "Registration successful! You can now login.";
+        $_SESSION['name'] = "";
+        $_SESSION['username'] = "";
+        $_SESSION['email'] = "";
+        $_SESSION['password'] = "";
+    } elseif ($status === "errUname") {
+        $_SESSION['invalidUnameMsg'] = "Username already exists";
+        $_SESSION['success'] = "";
+    } elseif ($status === "errEmail") {
+        $_SESSION['invalidEmailMsg'] = "Email already exists";
+        $_SESSION['success'] = "";
+    } else {
+        $_SESSION['invalidMsg'] = "Registration failed";
+    }
+
+    header("Location: ../views/reg.php");
+}
+?>
