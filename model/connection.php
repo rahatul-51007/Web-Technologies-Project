7450da536c113308527c933e0608c0b02319407a
hedayet-ullah-patwary <hedayetullahpatwary@gmail.com>
2026-01-18 21:44:45 +0600

    Add database connection

---
+++ model/connection.php
@@ -0,0 +1,17 @@
+<?php
+function dbConnection(){ 
+    $hostName="localhost";
+    $username="root";
+    $password="";
+    $dbName="hotel_management";
+    $conn=mysqli_connect($hostName,$username,$password,$dbName);
+    if($conn){
+        return $conn;
+    }
+    else{
+        echo "Connection failed";
+        die("Connection failed: " . mysqli_connect_error());
+        return false;
+    }
+}
+?>
